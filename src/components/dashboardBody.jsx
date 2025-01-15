bd191451f1ad1f75cd415921445a13cf293b2615
Israel Shedrack <israelshedrack913@gmail.com>
2025-01-15 17:14:22 +0100

    update: made new changes to dasboard and sidebar

--- src/components/dashboardBody.jsx
+++ src/components/dashboardBody.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { AiOutlineSend } from "react-icons/ai";
+import { FaNairaSign } from "react-icons/fa6";
+import { MdCallReceived } from "react-icons/md";
+
+
+
 
 function DashboardBody({tab}) {
 
@@ -10,16 +16,46 @@ function DashboardBody({tab}) {
         <section className='px-[20px]'>
 
           <div className='p-[40px]'>
-            <h2 className='urba font-semibold text-[1.5rem]'>Balance</h2>
+            <h2 className='urba font-semibold text-[1.5rem]'>Balance:  
+             
+                
+              <span className='text-[0.9rem] ml-[10px] '>
+                {"200000".toLocaleString()} 
+              </span>
+            </h2>
           </div>
 
           <div className='flex gap-[20px]'>
 
-            <div className='w-[100%] bg-black h-[300px] rounded-[20px]'>
+            <div className='w-[100%] bg-black h-[250px] rounded-[20px] p-[20px] text-[--white] flex flex-col justify-between'>
+
+              <div className='flex justify-between items-center text-[1.5rem]'>
+                <h3 className=' pop font-semibold'>Send</h3>
+
+                <i className=''>
+                  <AiOutlineSend />
+                </i>
+              </div>
+
+              <p className='text-[0.8rem] pop'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta necessitatibus totam id aliquam magnam quasi corporis ea quod maiores laborum facilis
+              </p>
 
             </div>
 
-            <div className='w-[100%] bg-[--gray] h-[300px]'>
+            <div className='w-[100%] bg-[--gray] h-[250px] rounded-[20px] p-[20px] text-[--black] flex flex-col justify-between'>
+
+              <div className='flex justify-between items-center text-[1.5rem]'>
+                <h3 className=' pop font-semibold'>Receive</h3>
+
+                <i className=''>
+                  <MdCallReceived />
+                </i>
+              </div>
+
+              <p className='text-[0.8rem] pop'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta necessitatibus totam id aliquam magnam quasi corporis ea quod maiores laborum facilis
+              </p>
 
             </div>
 
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { FaChalkboardUser } from "react-icons/fa6";
+import { FaWallet } from "react-icons/fa6";
+import { GrTransaction } from "react-icons/gr";
+import { TbReportSearch } from "react-icons/tb";
+
+
+
+
 
 
 function SideBar({tab}) {
@@ -7,31 +13,41 @@ function SideBar({tab}) {
 
 
   return (
-   <section className='h-screen w-[25%] bg-[--bg]'>
+   <section className='h-screen w-[25%] bg-[--bg] flex  flex-col justify-between '>
     <div className='flex items-center p-[20px]'>
         <h1 className='urba font-semibold text-[2rem]'>Scourge</h1>
     </div>
 
-    <ul className='flex flex-col gap-[10px] pop text-[1.2rem]'>
+    <ul className='flex flex-col gap-[10px] pop text-[1.2rem] px-[10px] h-[70vh]'>
         <li className='px-[20px] hover:bg-[--white] py-[10px] rounded-[30px] cursor-pointer'>
-            <button className='sm:hover:bg-[--white] '>
-                <i></i>
+            <button className='outline-none flex items-center gap-[10px]'>
+                <i className='text-[1.5rem]'>
+                    <FaWallet />
+                </i>
                 <p>Balance</p>
             </button>
         </li>
         <li className='px-[20px] hover:bg-[--white] py-[10px] rounded-[30px] cursor-pointer'>
-            <button>
-                <i></i>
+            <button className='outline-none flex items-center gap-[10px]'>
+                <i className='text-[1.5rem]'>
+                    <GrTransaction />
+                </i>
                 <p>Transactions</p>
             </button>
         </li>
         <li className='px-[20px] hover:bg-[--white] py-[10px] rounded-[30px] cursor-pointer'>
-            <button>
-                <i></i>
+            <button className='outline-none flex items-center gap-[10px]'>
+                <i className='text-[1.5rem] '>
+                    <TbReportSearch />
+                </i>
                 <p>Reports</p>
             </button>
         </li>
     </ul>
+
+    <figure className=''>
+        <img src="" className='w-[50px] h-[50px] p-[10px] bg-white rounded-full' alt="" />
+    </figure>
 
    </section>
   )
