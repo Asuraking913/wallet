b36ad530655e38f3c29f46df0f1a65c57dec8fec
Israel Shedrack <israelshedrack913@gmail.com>
2025-01-17 13:05:04 +0100

    update: made new changes to transactions page

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,6 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SubTransactions from './subTransactions'
 
 function Transactions() {
+
+  const [transList, setTransList] = useState([
+    {
+      reference: "2424234234", 
+      amount: "10000", 
+      prevBalance: "20000",
+      newBalance: "100000", 
+      date: "qwrqwrqwe"
+    }, 
+    {
+      reference: "2424234234", 
+      amount: "10000", 
+      prevBalance: "20000",
+      newBalance: "100000", 
+      date: "qwrqwrqwe"
+    }, 
+    {
+      reference: "2424234234", 
+      amount: "10000", 
+      prevBalance: "20000",
+      newBalance: "100000", 
+      date: "qwrqwrqwe"
+    }, 
+    {
+      reference: "2424234234", 
+      amount: "10000", 
+      prevBalance: "20000",
+      newBalance: "100000", 
+      date: "qwrqwrqwe"
+    }, 
+
+  ])
+
+  const transactions = transList.map((items, i) => <SubTransactions {...items} key={i}/>)
+
   return (
     <section className='flex flex-col gap-[10px] px-[20px]'>
 
@@ -9,6 +45,41 @@ function Transactions() {
       </div>
 
       <div className='border-[1px] border-[--gray]'></div>
+
+      <section className=' bg-[--gray] p-[10px] flex flex-col gap-[10px] rounded-[5px]'>
+
+        <h2 className='urba text-[1.2rem] font-semibold'>Transactions</h2>
+
+        <div className='border-[1px] border-[--white]'></div>
+
+        <article>
+
+          <ul className='flex justify-between pop capitalize p-[10px]'>
+            <li className='w-[150px]'>
+              Reference
+            </li>
+            <li className='w-[150px]'>
+              Amount
+            </li>
+            <li className='w-[150px] '>
+              Previous Balance
+            </li>
+            <li className='w-[150px] '>
+              New Balance
+            </li>
+            <li className='w-[150px] '>
+              Date
+            </li>
+          </ul>
+
+          <div className='flex flex-col gap-[30px] mt-[20px]'>
+            {transactions}
+          </div>
+
+        </article>
+
+
+      </section>
 
     </section>
   )
---
+++ src/components/subTransactions.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { TbCurrencyNaira } from "react-icons/tb";
+
+
+
+function SubTransactions(items) {
+  return (
+   <ul className='flex justify-between pop capitalize p-[10px] text-[0.9rem]'>
+        <li className='w-[150px]'>
+          {items.reference}
+        </li>
+        <li className='w-[150px] flex items-center gap-[2px]'>
+            <span className='text-[1.1rem]'>
+                <TbCurrencyNaira />
+            </span>
+
+            <span>
+                {items.amount}
+            </span>
+        </li>
+        <li className='w-[150px]'>
+          {items.prevBalance}
+        </li>
+        <li className='w-[150px]'>
+          {items.newBalance}
+        </li>
+        <li className='w-[150px]'>
+          {items.date}
+        </li>
+    </ul>
+  )
+}
+
+export default SubTransactions
