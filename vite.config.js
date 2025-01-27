d618db4d143da0ed9d7ba50cf30d841836fcf875
Israel Shedrack <israelshedrack913@gmail.com>
2025-01-27 11:43:51 +0100

    uupdate: deployed api on render

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://127.0.0.1:8000",
+//         changeOrigin: true,
+//         secure: true, // Set to true for HTTPS
+//       },
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "https://wallet-api-6med.onrender.com",
         changeOrigin: true,
         secure: true, // Set to true for HTTPS
       },
